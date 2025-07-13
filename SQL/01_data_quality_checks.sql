fc4aeb08cc742102f4c5f135ca7eebff74b31b15
Bhavitha Asam <bhavithaasam@gmail.com>
2025-07-13 16:37:57 -0400

    Add SQL quality checks for Medicare Part D oncology data

--- SQL/01_data_quality_checks.sql
+++ SQL/01_data_quality_checks.sql
@@ -299,4 +299,182 @@ WHERE
 SELECT COUNT(*) FROM oncology_drug_data_nonzero;
 
 
+--- long-format table ---
+CREATE OR REPLACE VIEW oncology_drug_data_long AS
+SELECT
+    id,
+    Brnd_Name,
+    Gnrc_Name,
+    Mftr_Name,
+    2019 AS Year,
+    Tot_Spndng_2019 AS Tot_Spndng,
+    Tot_Dsg_Unts_2019 AS Tot_Dsg_Unts,
+    Tot_Clms_2019 AS Tot_Clms,
+    Tot_Benes_2019 AS Tot_Benes,
+    Avg_Spnd_Per_Dsg_Unt_Wghtd_2019 AS Avg_Spnd_Per_Dsg_Unt_Wghtd,
+    Avg_Spnd_Per_Clm_2019 AS Avg_Spnd_Per_Clm,
+    Avg_Spnd_Per_Bene_2019 AS Avg_Spnd_Per_Bene,
+    Outlier_Flag_2019 AS Outlier_Flag
+FROM oncology_drug_data_nonzero
+
+UNION ALL
+
+SELECT
+    id,
+    Brnd_Name,
+    Gnrc_Name,
+    Mftr_Name,
+    2020 AS Year,
+    Tot_Spndng_2020,
+    Tot_Dsg_Unts_2020,
+    Tot_Clms_2020,
+    Tot_Benes_2020,
+    Avg_Spnd_Per_Dsg_Unt_Wghtd_2020,
+    Avg_Spnd_Per_Clm_2020,
+    Avg_Spnd_Per_Bene_2020,
+    Outlier_Flag_2020
+FROM oncology_drug_data_nonzero
+
+UNION ALL
+
+SELECT
+    id,
+    Brnd_Name,
+    Gnrc_Name,
+    Mftr_Name,
+    2021,
+    Tot_Spndng_2021,
+    Tot_Dsg_Unts_2021,
+    Tot_Clms_2021,
+    Tot_Benes_2021,
+    Avg_Spnd_Per_Dsg_Unt_Wghtd_2021,
+    Avg_Spnd_Per_Clm_2021,
+    Avg_Spnd_Per_Bene_2021,
+    Outlier_Flag_2021
+FROM oncology_drug_data_nonzero
+
+UNION ALL
+
+SELECT
+    id,
+    Brnd_Name,
+    Gnrc_Name,
+    Mftr_Name,
+    2022,
+    Tot_Spndng_2022,
+    Tot_Dsg_Unts_2022,
+    Tot_Clms_2022,
+    Tot_Benes_2022,
+    Avg_Spnd_Per_Dsg_Unt_Wghtd_2022,
+    Avg_Spnd_Per_Clm_2022,
+    Avg_Spnd_Per_Bene_2022,
+    Outlier_Flag_2022
+FROM oncology_drug_data_nonzero
+
+UNION ALL
+
+SELECT
+    id,
+    Brnd_Name,
+    Gnrc_Name,
+    Mftr_Name,
+    2023,
+    Tot_Spndng_2023,
+    Tot_Dsg_Unts_2023,
+    Tot_Clms_2023,
+    Tot_Benes_2023,
+    Avg_Spnd_Per_Dsg_Unt_Wghtd_2023,
+    Avg_Spnd_Per_Clm_2023,
+    Avg_Spnd_Per_Bene_2023,
+    Outlier_Flag_2023
+FROM oncology_drug_data_nonzero;
+
+SELECT * FROM oncology_drug_data_long LIMIT 20;
+
+CREATE OR REPLACE VIEW oncology_drug_data_long_oncology AS
+SELECT *
+FROM oncology_drug_data_long
+WHERE Brnd_Name IN (
+    'Abraxane',
+    'Afinitor',
+    'Afinitor Disperz',
+    'Alecensa',
+    'Alimta',
+    'Alunbrig',
+    'Arimidex',
+    'Aromasin',
+    'Avastin',
+    'Azacitidine',
+    'Bendamustine',
+    'Bendeka',
+    'Bicalutamide',
+    'Bosulif',
+    'Bortezomib',
+    'Cabometyx',
+    'Calquence',
+    'Caprelsa',
+    'Carboplatin',
+    'Casodex',
+    'Cisplatin',
+    'Copiktra',
+    'Cyramza',
+    'Darzalex',
+    'Decitabine',
+    'Docetaxel',
+    'Doxorubicin HCl',
+    'Doxorubicin HCl Liposome',
+    'Erbitux',
+    'Erleada',
+    'Erlotinib HCl',
+    'Etoposide',
+    'Everolimus',
+    'Exemestane',
+    'Faslodex',
+    'Femara',
+    'Fluorouracil',
+    'Fulvestrant',
+    'Gazyva',
+    'Gemcitabine HCl',
+    'Gleevec'
+)
+OR Gnrc_Name IN (
+    'Abiraterone Acetate',
+    'Azacitidine',
+    'Bendamustine',
+    'Bicalutamide',
+    'Bortezomib',
+    'Cabozantinib',
+    'Calquence',
+    'Carboplatin',
+    'Cisplatin',
+    'Cyclophosphamide',
+    'Cytarabine',
+    'Decitabine',
+    'Docetaxel',
+    'Doxorubicin HCl',
+    'Erlotinib',
+    'Etoposide',
+    'Everolimus',
+    'Exemestane',
+    'Fluorouracil',
+    'Fulvestrant',
+    'Gemcitabine',
+    'Imatinib'
+);
+
+SELECT * FROM oncology_drug_data_long_oncology LIMIT 20;
+
+SHOW VARIABLES LIKE 'secure_file_priv';
+
+SELECT * FROM oncology_drug_data_long_oncology;
+
+
+--- Export the Cleaned Oncology Data---
+SELECT *
+INTO OUTFILE '/Users/bhavithaasam/Desktop/Projects/Medicare Part D Spending/Medicare Part D Spending by Drug/Dataset/DSD_PTD_RY25_P04_V10_DY23_BGM.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+FROM oncology_drug_data_long_oncology;
+
 
